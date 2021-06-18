d39387d30593256857f3ea90aae2b103040d07f0
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-06-18 14:55:17 -0400

    set up Users From to create a User

--- src/actions/usersAction.js
+++ src/actions/usersAction.js
@@ -2,8 +2,9 @@ const url = "http://127.0.0.1:3001/users"
 
 const setUsers = (users) => ({type: "GOT_USERS", payload: users})
 
-export const fetchUsers = () => {
+const addUser = (user) => ({type: 'ADD_USER', payload: user})
 
+export const fetchUsers = () => {
   return (dispatch) => { 
     fetch(url)
     .then(resp => resp.json())
@@ -11,6 +12,26 @@ export const fetchUsers = () => {
       dispatch(setUsers(users))
     })
   }
-
-
 }
+
+export const createUser = (user) => {
+  return (dispatch) => {
+    const configObj = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accepts": "application/json"
+      },
+      body: JSON.stringify(user)
+    }
+
+    fetch(url, configObj)
+    .then(resp => resp.json())
+    .then(user => {
+      dispatch(addUser(user))
+    })
+  }
+}
+
+
+
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import {createUser} from '../actions/usersAction'
+import {connect} from 'react-redux'
 
 
 class UserForm extends Component {
@@ -9,14 +11,36 @@ class UserForm extends Component {
     password: ""
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.createUser(this.state)
+    this.setState({
+      name: "",
+      email: "",
+      password: ""
+    })
+  }
+
   render() {
     return(
-      <form>
-        <label>cheese</label>
+      <form onSubmit={this.handleSubmit}>
+        <label >UserName:</label>
+        <input onChange={this.handleChange} type="text" name="name" value={this.state.name} />
+        <label>Email:</label>
+        <input onChange={this.handleChange} type="email" name="email" value={this.state.email} />
+        <label>Password:</label>
+        <input onChange={this.handleChange} type="password" name="password" value={this.state.password} />
+        <input type="submit" />
       </form>
     )
   }
 
 }
 
-export default UserForm
+export default connect(null, {createUser})(UserForm)
