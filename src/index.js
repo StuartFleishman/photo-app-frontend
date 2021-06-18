a2d8aa4c1b76d2753b2da6c967218b97e9e6e12c
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-06-18 07:28:06 -0400

    created store and usersReducer

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk'
+import usersReducer from './reducer/usersReducer'
+
+const store = createStore(usersReducer, applyMiddleware(thunk))
+
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
