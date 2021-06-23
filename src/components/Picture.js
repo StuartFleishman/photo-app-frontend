54d92ae3c39b971ba54abc128b44b730e2449ec3
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-06-23 17:15:19 -0400

    created like button for pictures

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const Picture = ({title, image}) => {
+const Picture = ({title, image, likes}) => {
 
   return (
     <div>
       <ul>
       <li>{title}</li>
       <img src={image} alt={title} height={200} width={200} />
+        <p>{likes}</p>
+      <button >Like</button>
       </ul>
     </div>
   )
